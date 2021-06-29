13c2dd1b360413a469ebfdc742368fb562ccee94
Weslley Braga <wesbragagt@gmail.com>
2021-06-29 01:02:03 -0500

    push

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
-// const baseURL = process.env.REACT_APP_MOCK ? '/' : (process.env.REACT_APP_API_URL || 'http://localhost:8080')
-const baseURL = 'http://localhost:8080'
+const baseURL = process.env.REACT_APP_MOCK ? '/' : (process.env.REACT_APP_API_URL || 'http://localhost:8080')
+// const baseURL = 'http://localhost:8080'
 const domain = process.env.REACT_APP_DOMAIN || ''
 const clientId = process.env.REACT_APP_CLIENT_ID || ''
 
